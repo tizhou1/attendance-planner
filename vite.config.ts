aa9cc012bd846cba659e358ce7b194e3462d6f9e
Tian Zhou <tizhou@expediagroup.com>
2025-10-30 14:53:21 -0700

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/attendance-planner/',
+  base: '/attendance-planner',
   plugins: [react()],
 })
